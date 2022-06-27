594f5cf727cf119f067ce1993c9da4107132c4d1
DeveloperKGH <developerkgh@gmail.com>
2022-06-27 10:47:06 +0900

    :white_check_mark: "add test for service" - article.service.spec.ts : 테스트 코드 추가

--- src/article/domain/port/article.repository.ts
+++ src/article/domain/port/article.repository.ts
@@ -5,4 +5,10 @@ export interface ArticleRepository {
   findAll(): Promise<Article[] | null>;
 }
 
+/* Typescript 사용 시 Interface 를 활용하여 의존성 주입
+ * 인터페이스는 Javascript 의 것이 아니므로, 런타임 시 인터페이스가 사라진다.
+ * 따라서 IOC 컨테이너가 인터페이스 기반으로 의존성 주입할때, 무엇을 주입해야하는지 모름
+ * 해결 방법 : Interface 를 string/symbol 로 대체한다.
+ * IOC 컨테이너가 symbol 키맵을 이용하여 class 정보를 관리하는 것으로 생각된다.
+ * */
 export const ArticleRepository = Symbol('ArticleRepository'); //TODO : Symbol 용도 관련 R&D 필요
---
+++ src/article/test/article.service.spec.ts
@@ -0,0 +1,108 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ArticleService } from '../domain/port/article.service';
+import { Article } from '../domain/model/article.model';
+import { ArticleRepository } from '../domain/port/article.repository';
+import { ArticleMongoRepository } from '../adapter/db/articleMongo.repository';
+import { TypegooseModule } from 'nestjs-typegoose';
+import * as XLSX from 'xlsx';
+
+describe('ArticleService', () => {
+  let service: ArticleService;
+  let repository: ArticleRepository;
+  let article1: Article;
+  let article2: Article;
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypegooseModule.forRoot('mongodb://localhost:27017/board'),
+        TypegooseModule.forFeature([Article]),
+      ],
+      providers: [
+        ArticleService,
+        { provide: ArticleRepository, useClass: ArticleMongoRepository },
+      ],
+    }).compile();
+
+    service = module.get<ArticleService>(ArticleService);
+    repository = module.get<ArticleRepository>(ArticleRepository);
+
+    article1 = new Article();
+    article2 = new Article();
+  });
+
+  describe('게시물 등록', () => {
+    it('게시물을 생성하고, 생성 된 게시물 return', async () => {
+      //Given
+      article1.title = '제목';
+      article1.content = '본문';
+
+      //jest.spyOn 으로 repository save 함수 호출을 모의하고, 이 모의된 함수는 mockResolvedValue 를 사용해서 모의 저장된 Article 반환
+      const repositorySaveSpy = jest
+        .spyOn(repository, 'save')
+        .mockResolvedValue(article1);
+
+      //When
+      const result = await service.createArticle(article1);
+
+      //Then
+      expect(repositorySaveSpy).toHaveBeenCalledWith(article1); // 모의 함수가 특정 인수로 호출되었는지 확인하는 데 사용
+      expect(result).toEqual(article1);
+    });
+  });
+
+  describe('게시물 엑셀 다운로드', () => {
+    it('게시물 목록을 조회하고, 조회 된 결과를 엑셀 파일로 return', async () => {
+      //Given
+      article1.title = '제목1';
+      article1.content = '본문1';
+
+      article2.title = '제목2';
+      article2.content = '본문2';
+
+      const existingArticles = [article1, article2];
+
+      //jest.spyOn 으로 repository findAll 함수 호출을 모의하고, 이 모의된 함수는 mockResolvedValue 를 사용해서 existingArticles 반환
+      const repositoryFindAllSpy = jest
+        .spyOn(repository, 'findAll')
+        .mockResolvedValue(existingArticles);
+
+      //When
+      const result = await service.downloadExcel();
+
+      //Then
+      const workbook = XLSX.read(result);
+      const sheetName = workbook.SheetNames[0];
+      const sheet = workbook.Sheets[sheetName];
+
+      expect(repositoryFindAllSpy).toBeCalled();
+      expect(sheetName).toEqual('게시물');
+      expect(XLSX.utils.sheet_to_json(sheet)).toEqual(existingArticles);
+    });
+  });
+
+  describe('게시물 목록 조회', () => {
+    it('게시물 목록을 조회하고, 조회 된 결과를 return', async () => {
+      //Given
+      article1.title = '제목1';
+      article1.content = '본문1';
+
+      article2.title = '제목2';
+      article2.content = '본문2';
+
+      const existingArticles = [article1, article2];
+
+      //jest.spyOn 으로 repository findAll 함수 호출을 모의하고, 이 모의된 함수는 mockResolvedValue 를 사용해서 existingArticles 반환
+      const repositoryFindAllSpy = jest
+        .spyOn(repository, 'findAll')
+        .mockResolvedValue(existingArticles);
+
+      //When
+      const results = await service.getArticles();
+
+      //Then
+      expect(repositoryFindAllSpy).toBeCalled(); // 모의 함수가 호출되었는지 확인하는 데 사용
+      expect(results).toEqual(existingArticles);
+    });
+  });
+});
